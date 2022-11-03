021b3cfd0d9d14f584ea33b3e00a95af7e51ab3d
DAM <DAM@203-14>
2022-11-03 15:24:55 +0100

    He arreglat les faltes

---
+++ src/main/java/com/mycompany/conversio/Conversio.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.conversio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nil
+ */
+public class Conversio {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introdueix en €");
+        int valor_euros = sc.nextInt();
+                
+        double tipus_de_canvi = 1.15d;
+        System.out.println(valor_euros + " eur0s (€) acatualmentt equivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+    }
+}
